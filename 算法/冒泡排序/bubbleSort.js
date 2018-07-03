c79681a33a309b92256672c482548354ec9071be
starsion <starsion@outlook.com>
2018-07-03 20:39:49 +0800

    add some file Signed-off-by: starsion <starsion@outlook.com>

--- 算法/冒泡排序/bubbleSort.js
+++ 算法/冒泡排序/bubbleSort.js
@@ -2,7 +2,6 @@ var array = [5, 9, 6, 5, 3, 1]
 var arrayLength = array.length
 for (let i = arrayLength - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
-        // console.log(array[j], array[j + 1])
         if (array[j] > array[j + 1]) {
             let temp = array[j]
             array[j] = array[j + 1]
@@ -13,4 +12,4 @@ for (let i = arrayLength - 1; i > 0; i--) {
     console.log("-------")
 }
 
-console.log(array)
+console.log("result:",array)
